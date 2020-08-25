cd5104cc913d6974760b8f9e0ab54d208d0f897d
JasemDuncan <jasemvalencia@gmail.com>
2020-08-25 12:22:02 -0500

    Tagging feature completed

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,4 +2,13 @@ class TagsController < ApplicationController
     def show
         @tag = Tag.find(params[:id])
     end
+    def index
+        @articles = Article.all
+    end
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+    
+        redirect_to articles_path(index)
+    end
 end
